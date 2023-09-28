7be5ae108684ee3adb83c56ff0c6a69d7961961a
EwoutBergsma <43827037+EwoutBergsma@users.noreply.github.com>
2023-09-28 20:12:56 +0200

    Update test_exercise.py
    Copied Nima's and Ali's unit tests

--- test/test_exercise.py
+++ test/test_exercise.py
@@ -3,6 +3,8 @@ import pytest
 from testbook import testbook
 import random
 import os
+import numpy as np  # Add this import statement to resolve the NameError
+
 
 # This is to prevent: RuntimeWarning: Proactor event loop does not implement add_reader family of methods required for zmq. Registering an additional selector thread for add_reader support via tornado.
 if os.name == 'nt':  # Check if running on a Windows machine
@@ -18,14 +20,42 @@ def tb():
 
 ################### The actual tests ###################
 
+# For the exercise 1:
 # order: frequency, amplitude, phase_shift, sample_rate, n_seconds
-# input_1 = [1, 1, 0, 44100, 1]
-# output_1 = ([0.0,  0.1,  0.2,  0.3,  0.4,  0.5,  0.6,  0.7,  0.8,  0.9], [0.0,  0.5877852522924731,  0.9510565162951535,  0.9510565162951535,  0.5877852522924732,  1.2246467991473532e-16,  -0.5877852522924734,  -0.9510565162951535,  -0.9510565162951536,  -0.5877852522924734])
+input_1 = [1, 1, 1, 2, 3]
+output_1 = ([0.0, 0.5, 1.0, 1.5, 2.0, 2.5], [-0.8414709848078965, -0.5403023058681395, 0.8414709848078967, 0.5403023058681392, -0.841470984807897, -0.5403023058681374])
 
-input_1 = [1, 1, 1, 1, 1]
-output_1 = ([3, 4], [5, 6])
 
-@pytest.mark.parametrize("test_input,expected", [(input_1, output_1)]) # , ([3.5, 7.13], 10.63), ([-13.23, 6], -7.23)])
+# Helper function to convert numpy arrays to Python lists
+def convert_arrays_to_lists(arr):
+    if isinstance(arr, np.ndarray):
+        return arr.tolist()
+    elif isinstance(arr, list):
+        return [convert_arrays_to_lists(item) for item in arr]
+    else:
+        return arr
+
+# Modified test function
+@pytest.mark.parametrize("test_input,expected", [(input_1, output_1)])
 def test_exercise_1(tb, test_input, expected):
-    student_work = tb.ref("get_sine_data")
-    assert student_work(frequency=test_input[0], amplitude=test_input[1], phase_shift=test_input[2], sample_rate=test_input[3], n_seconds=test_input[4]) == pytest.approx(expected, 0.01)
+    student_work1 = tb.ref("get_sine_data")
+    actual = student_work1(frequency=test_input[0], amplitude=test_input[1], phase_shift=test_input[2], sample_rate=test_input[3], n_seconds=test_input[4])
+    expected = convert_arrays_to_lists(expected)  # Convert expected to Python lists
+
+
+# For exercise 2:
+
+input_2 = ([1, 1, 1, 2, 3],[1, 2, 2, 2, 5])
+output_2 = [1, 3, 5, 8, 16, 17, 15, 16, 15]
+
+
+
+@pytest.mark.parametrize("test_input,expected", [(input_2, output_2)])
+def test_exercise_2(tb, test_input, expected):
+    student_work2 = tb.ref("my_convolution")
+    actual = student_work2(a=test_input[0], b=test_input[1])
+    expected = convert_arrays_to_lists(expected)  # Convert expected to Python lists
+
+
+
+
